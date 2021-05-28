667cf7807055095798d80c35fe9ae13ab7ca4abe
Ng Wei Hong <wngg0002@student.monash.edu>
2021-05-28 12:07:38 +0800

    Update extract ten sentence.py
    Organized code to greatly increase code readability
    
    Code is now fully documented

--- extract ten sentence.py
+++ extract ten sentence.py
@@ -1,133 +1,89 @@
-import sys
-import csv
-
-from computeARIintoANewLastColumn import readfile
-
-# TODO:
-#       document code properly, and also testing
-#           remember to document each class and function, with complexity, input, return, etc
-#           (btw need ot double check if preconditions are documented correctly)
-#           error handling//catching, such as try and except
-### the use of "###" in comments indicates the comment is means as a NOTE, or a possible place to TEST, or A PROBLEM
-### THAT NEEDS ADDRESSING
+import unittest
 
 
-
-# Extra code limitation: "....." is
-
-
-def main_function(dataset_file_name):
-    """
-    This functions extracts only the first 10 sentence in the csv only, and returns the data now holding
-     only 10 sentences.
-    :precondition: dataset must be .csv
-                   the text data must be in the last row for our dataset
-    :param dataset_file_name: the .csv dataset's file name
-    :return: tuple (header, data)
-                WHERE
-                    header is the first header row to be added to our output csv in list format
-
-                    data is the rows to be added to our output csv in list format, and each row in data
-                    represents a document
-    """
-
-    # read our csv file
-    header, data = readfile(dataset_file_name)
-
-    # m is the index of the last cell of in a row
-    # we assume all row are has the same number of columns in the CSV
-    m = len(data[0]) - 1
-
-    # note that each row is assumed to represent a document
-    # for each document, tokenize its text
-    the_extracted = []
-    for row in data:
-        text = row[m]
-        row[m] = (extract(text))
+from extractTenSentence import *
 
 
-    return header, data
+class TestTheMainFunction(unittest.TestCase):
+
+    def test_the_main_non_existent_file(self):
+        """
+        Test to make sure it throws error as expected with non_existent_file
+        """
+        with self.assertRaises(SystemExit):
+            the_main_function("non existent file")
+
+    def test_the_main_empty_file(self):
+        """
+        Test to make sure it throws error as expected with empty_file
+        """
+        with self.assertRaises(SystemExit):
+            the_main_function("testfile_empty_file.csv")
+
+    def test_the_main_non_csv_file(self):
+        """
+        Test to make sure it throws error as expected with non_csv file
+        """
+        with self.assertRaises(SystemExit):
+            the_main_function("test_file_not_a_csv")
+
+    def test_the_main_with_non_utf_file(self):
+        """
+        Test to make sure it throws error as expected with non-utf8 file
+        """
+        with self.assertRaises(SystemExit):
+            the_main_function('test_non_utf_8.csv.csv')
+
+    def test_the_main_with_simple_yet_proper_file(self):
+        """
+        Test for it running as expected with proper file
+        """
+        assert the_main_function('test_proper_file.csv') == (['\ufeffid', 'link'], [['1', 'abc.com'], ['2', 'gef.com']])
+
+    def test_the_main_with_proper_file_two(self):
+        """
+        Test for it running as expected with proper file two
+        """
+        assert the_main_function('test_proper_file_2.csv') == (['\ufeffUnique_ID', 'Link', 'Title', 'Text'], [['', 'https://www.timeforkids.com/k1/ready-set-recycle-2/', 'Ready, Set, Recycle!', 'On Earth Day, people all around the world help the planet. But you can help the planet anytime. Recycling is an important way to do this. Do you recycle? Read on to learn more about it.\n\nWhat Can I Recycle?\n\nPaper can be recycled. So can metal, plastic, and glass. Give those a rinse first.\n\nKeep It Separated\n\nPut each material into a different bin.'], ['', 'a', 'c', 'apple is of the color red.']])
+
+    def test_the_main_with_proper_file_three(self):
+        """
+        Test for it running as expected with proper file three
+        """
+        assert the_main_function('test_proper_file_3.csv') == (['\ufeffUnique_ID', 'Link', 'Title', 'Text'], [['', 'https://www.timeforkids.com/k1/ready-set-recycle-2/', 'Ready, Set, Recycle!', 'On Earth Day, people all around the world help the planet. But you can help the planet anytime. Recycling is an important way to do this. Do you recycle? Read on to learn more about it.\n\nWhat Can I Recycle?\n\nPaper can be recycled. So can metal, plastic, and glass. Give those a rinse first.\n\nKeep It Separated\n\nPut each material into a different bin.'], ['', 'https://www.timeforkids.com/k1/lets-compost/', "Let's Compost!", 'This Earth Day, why not make a compost bin? Put some soil in a plastic bin or tub. Add leaves and twigs. Moisten with water. Now you can add scraps of fruits and vegetables. You can also add coffee grounds and old tea bags.\n\nHow does composting work? Tiny creatures found in soil will feast on the food scraps. They will break them down. The soil will become rich with nutrients.'], ['', 'https://www.timeforkids.com/k1/be-here-now-2/', 'Be Here Now', 'Mindfulness is taught in many schools. Kids learn deep breathing, movement, and how to relax. Mindfulness can help people in different ways. Read on to learn more.\n\nCalm Down\n\nMindfulness helps people relax. It calms the body. It also quiets the mind.\n\nBe Happy\n\nMindfulness helps people feel good. It reduces anger and sadness. It is said to boost happiness.']])
 
 
-def extract(text):
+class TestExtract(unittest.TestCase):
+
+    def test_extract_when_input_is_less_than_ten_sentences(self):
+        """
+        test for when input is less than ten sentences
+        """
+        assert (extract('this is a sentence. this too. 3122313 and these as well. four. five. six.')) == 'this is a sentence. this too. 3122313 and these as well. four. five. six.'
+
+    def test_extract_when_input_is_more_than_ten_sentences(self):
+        assert (extract('this is a sentence. this too. 3122313 and these as well. four. five. six. seven. eight. nine. ten. eleven. sentences.')) == 'this is a sentence. this too. 3122313 and these as well. four. five. six. seven. eight. nine. ten.'
+
+    def test_extract_when_there_are_newlines(self):
+        assert (extract('this is a sentence. this too. 3122313 and these as well. \n\n\n\nfour. five. six. seven. eight. nine. ten. eleven. sentences.')) == 'this is a sentence. this too. 3122313 and these as well. \n\n\n\nfour. five. six. seven. eight. nine. ten.'
 
 
-    sentence_count = 0 # there is 0 sentence extracted in the beginning
-    sentence = ""
-    i = 0
+class TestIncreaseSentenceCountIfWeShould(unittest.TestCase):
 
-    for character in text:
+    def test_for_correct_updating_one(self):
+        """
+        check if it updates correctly when it should
+        """
+        assert increase_sentence_count_if_we_should('one. two. three.', 3, 'a') == (4, 'one. two. three.a')
 
-        # if it is an alphabet, we continue counting the word's length
-        if character.isalpha():
-
-            sentence = sentence + character
-
-        ### A PROBLEM: HOW DO WE DIFFERENTIATE DECIMALS IN NUMBERS FROM PERIODS? NEED TO IMPLEMENT?
-        # if it is a period, that means it is the end of sentence
-        #           so we store the length of each words, in the sentence, in sentence token
-        elif (character == ".") or (character == "!") or (character == "?"): # newline is not considered another sentence
-
-            ### WATCH   # for handling decimal cases, since "." in decimals arent supposed to be treated as the end of sentence.
-            if character == ".":
-                if (i - 1) >= 0:  # preventing errors from checking index that does not exist
-                    if isinstance(text[i - 1], int):
-                        if (i - 1) < len(text):
-                            if isinstance(text[i + 1], int):
-                                # if it is actually a decimal, it should not be treated as end of sentence,
-                                # so we just continue scanning with the text
-
-                                sentence = sentence + character
-
-                                continue
+    def test_for_correct_updating_two(self):
+        """
+        check if it doesnt update when it should not.
+        """
+        assert increase_sentence_count_if_we_should('one. two. three. four.five.six.seven.eight. nine. ten.', 10, 'a') \
+               == (10, 'one. two. three. four.five.six.seven.eight. nine. ten.')
 
 
-            sentence_count, sentence = increase_sentence_count_if_we_should(sentence, sentence_count, character)
+if __name__ == '__main__':
 
-            # if it is not an alphabet, or a period,
-        else:
-
-            sentence = sentence + character
-
-
-        i += 1
-
-        # if we have 10 sentences extracted already, return these 10
-        if sentence_count == 10: # 9 cuz we already have 1 sentence in the beginning, and is a
-            return sentence
-
-    # for if the text have less than 10 sentences
-    increase_sentence_count_if_we_should(sentence, sentence_count, "") # not keeping any output since we dont arent using anymore outputs anyway
-
-    return sentence
-
-
-def increase_sentence_count_if_we_should(sentence, sentence_count, character):
-
-
-
-    # sentence length being 0 means there is no sentence to add
-    # because sentences that have no words are not considered a sentence
-    if (len(sentence) > 0) and (sentence_count < 10):
-        sentence = sentence + character
-        sentence_count = sentence_count + 1
-
-    return sentence_count, sentence
-
-
-if __name__ == "__main__":
-
-    the_dataset_file_name = sys.argv[1]  # line is for taking inputs from cmd, if we are using cmd
-
-    d_header, d_data = main_function(the_dataset_file_name)
-
-
-    # write our output csv
-    output_file_name = "ten_sentences_extracted" + " " + the_dataset_file_name
-    # write our new csv, with ARI added as a new last column, as output
-    with open(output_file_name, mode='w', newline='', encoding= "utf-8") as writing_file:
-        file_writer = csv.writer(writing_file, delimiter=',')
-
-        file_writer.writerow(d_header)
-        for row in d_data:
-            file_writer.writerow(row)
+    unittest.main()
