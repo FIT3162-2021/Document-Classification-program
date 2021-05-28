22b750dae82664c57b1e6d54e5e22b516f0a04c9
Ng Wei Hong <wngg0002@student.monash.edu>
2021-05-28 11:40:27 +0800

    Update computeARIintoANewLastColumn.py
    Edited documentations, code is now fully documented.
    
    Also, greatly improved code readability by organizing code.

--- computeARIintoANewLastColumn.py
+++ computeARIintoANewLastColumn.py
@@ -1,40 +1,82 @@
 import sys
 import csv
+import os
 
-# TODO:
-#       document code properly, and also testing
-#           remember to document each class and function, with complexity, input, return, etc
-#           (btw need ot double check if preconditions are documented correctly)
-#           error handling//catching, such as try and except
-### the use of "###" in comments indicates the comment is means as a NOTE, or a possible place to TEST, or A PROBLEM
-### THAT NEEDS ADDRESSING
-
-###### BTW, are we better off using regular expression? it is way more efficient to code now that i realize it. as for run time with regex, its probably the same
-
-###### BTW, do we need to explain what the ARI scores actually are in documentation, and also what the classification from the ARI scores mean?
-
-### NOTE: OUR FORMULA FOR ARI SCORE IS DIFFERENT FROM THE ONE IN WIKIPEDIA(AND POTENTIALLY SOME OTHER SOURCES):
-# OUR FORMULA IS AS BELOW
-# GL = 0.50(w/s) + 4.71(s/w) - 21.43
-# where GL = assigned grade level
-# w/s = words per sentence or sentence length,
-# s/w = characters per word or word length.
+# NOTE:
+# ***input file must be (.csv), with utf-8 encoding, for the program to run.
+#
+# ***input file should contain header. because the first row of the csv file will be considered as header.
+#
+# ***OUR ARI FORMULA IS AS BELOW:
+#
+#             ari_score = 0.50(w/s) + 4.71(s/w) - 21.43
+#             where ari_score = assigned grade level
+#             w/s = words per sentence or sentence length,
+#             s/w = characters per word or word length.
+#             define Python user-defined exceptions
 
 
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
 
+
+class EmptyFileError(Error):
+    """Raised when the the file is empty"""
+    pass
+
+
+class NotCsvFileError(Error):
+    """Raised when the the file is not csv, when the functions expects .csv file type"""
+    pass
 
 
 def readfile(dataset_file_name):
     """
     This functions opens the csv file with the input file name, and returns its header and rows.
-    :precondition: It expects the file to be opened as a .csv file, with "utf 8" encoding.
+    :precondition: It expects the file to be opened as a .csv file, with "utf 8" encoding. Must not be an empty file.
     :param dataset_file_name: the .csv dataset's file name
+    :complexity: O(m), where m is length of the file.
     :return: two tuple (f_header, f_row)
                 WHERE
                 list f_header is the list containing header data of the csv
 
                 list f_row is the list containing data of all rows in the csv, excluding the header
     """
+
+    # error handling, checking for empty file or non existent file, and also for .csv file type
+    try:
+
+        # test for if it is file exists
+        if os.stat(dataset_file_name).st_size > 0:
+            print("Reading file...")
+        else:
+            raise EmptyFileError
+
+        # test for if it is csv file type
+        name, extension = os.path.splitext(dataset_file_name)
+        if extension\
+                == ".csv":
+            print("Reading file...")
+        else:
+            raise NotCsvFileError
+
+        # our program expects only utf-8, so we test for encodings
+        test_reader = open(dataset_file_name, "r", encoding='utf-8')
+        next(test_reader)    # this is necessary because the error is only thrown when running next()
+        test_reader.close()
+
+    except OSError as e:
+        raise SystemExit("URL file missing ... exiting".format(e)) from None
+    except EmptyFileError as e:
+        raise SystemExit("Empty URL file ... exiting".format(e)) from None
+    except NotCsvFileError as e:
+        raise SystemExit("Expected .csv file, a file that is not .csv has been received ... exiting".format(e)) from \
+            None
+    except UnicodeDecodeError as e:
+        test_reader.close()
+        raise SystemExit("File needs to be in utf-8 encoding ... exiting".format(e)) from None
+
     f_data = []
 
     # opens our file, assuming our encoding as "utf-8"  ### TEST? NON "utf-8" may throw errors
@@ -45,9 +87,10 @@ def readfile(dataset_file_name):
         # set header data for returning
         f_header = next(opened_file)
 
-        for row in opened_file:
-            f_data.append(row)
+        for row_values in opened_file:
+            f_data.append(row_values)
 
+    print("Finished reading file.")
     return f_header, f_data
 
 
@@ -56,14 +99,21 @@ def tokenize(text):
     This function takes the text, tokenizes each of its sentence as a sentence token, and stores each words in the
     sentence as a world length in the sentence token.
     :param text: the string text to be tokenized
+    :complexity: O(n), where m is length of text.
     :return: list token
             WHERE
                 token is a list of sentence_tokens, each sentence in the input text is stored as one sentence_token,
                     each sentence_token stores each of its word as integer word_length
     """
 
+    # test to make sure input is string, function is meant to only work with string input
+    try:
+        if not isinstance(text, str):
+            raise TypeError
+    except TypeError as e:
+        raise SystemExit("The last row of the file, must be in string format ... exiting".format(e)) from None
 
-    ### NOTE: "sentences" that have no words are not considered a "sentence"
+    # NOTE: "sentences" that have no words are not considered a "sentence"
     token = []  # the token that will store sentence_tokens is initialised here
     word_length = 0
     sentence_tokens = []  # initialise
@@ -75,23 +125,22 @@ def tokenize(text):
 
             word_length += 1
 
-### A PROBLEM: HOW DO WE DIFFERENTIATE DECIMALS IN NUMBERS FROM PERIODS? NEED TO IMPLEMENT?
         # if it is a period, that means it is the end of sentence
-        #           so we store the length of each words, in the sentence, in senttoken
+        #           so we store the length of each words, in the sentence, in sentence_tokens
         elif (character == ".") or (character == "!") or (character == "?") or (character == "\n"):
 
-### WATCH   # for handling decimal cases, since "." in decimals arent supposed to be treated as the end of sentence.
             if character == ".":
-                if (i-1) >= 0: # preventing errors from checking index that does not exist
-                    if isinstance(text[i-1], int):
-                        if (i-1) < len(text):
-                            if isinstance(text[i+1], int):
+                if (i-1) >= 0:  # preventing errors from checking index that does not exist
+                    if text[i-1].isdigit():
+                        if (i+1) < len(text):
+                            if text[i+1].isdigit():
                                 # if it is actually a decimal, it should not be treated as end of sentence,
                                 # so we just continue scanning with the text
 
                                 continue
 
-            check_if_there_is_anything_to_add_if_there_is_then_add_sentence_token_to_dtoken(sentence_tokens, word_length, token)
+            check_if_there_is_anything_to_add_if_there_is_then_add_sentence_token_to_dtoken(sentence_tokens,
+                                                                                            word_length, token)
             word_length = 0
             sentence_tokens = []
 
@@ -113,7 +162,8 @@ def tokenize(text):
     return token
 
 
-def check_if_there_is_anything_to_add_if_there_is_then_add_sentence_token_to_dtoken(sentence_tokens, word_length, dtoken):
+def check_if_there_is_anything_to_add_if_there_is_then_add_sentence_token_to_dtoken(sentence_tokens,
+                                                                                    word_length, dtoken):
     """
     The functions checks if there is word_length to add to the sentence token, and if there is, add it. It then checks
     for if there is a sentence_token that should be added to our token, if there is, add it
@@ -121,8 +171,10 @@ def check_if_there_is_anything_to_add_if_there_is_then_add_sentence_token_to_dto
     sentence it represents
     :param word_length: the integer word_length at the current step of iteration
     :param dtoken: the list token that stores sentence_tokens for the document
+    :complexity: O(1)
     :return:
             None
+    :postcondition: dtoken gets updated accordingly
     """
     # function does exactly what it name says it does
 
@@ -139,10 +191,11 @@ def check_if_there_is_anything_to_add_if_there_is_then_add_sentence_token_to_dto
     return
 
 
-def get_classification(ARI_score):
+def get_classification(ari_score):
     """
     This function takes the text's ARI score, and returns its classification for the text
-    :param ARI_score: integer ARI score
+    :param ari_score: integer ARI score
+    :complexity: O(1)
     :return: integer classification
         where
                 # Classification:
@@ -154,15 +207,15 @@ def get_classification(ARI_score):
                 # 13+:expert  ->stored as integer 5
     """
 
-    if ARI_score < 4: # if value is between 0 ~ 3.999999999
+    if ari_score < 4:  # if value is between 0 ~ 3.999999999
         return 1
-    elif (ARI_score >= 4) & (ARI_score < 7): # if value is between 4 ~ 6.999999999
+    elif (ari_score >= 4) & (ari_score < 7):  # if value is between 4 ~ 6.999999999
         return 2
-    elif (ARI_score >= 7) & (ARI_score < 10): # if value is between 7 ~ 9.999999999
+    elif (ari_score >= 7) & (ari_score < 10):  # if value is between 7 ~ 9.999999999
         return 3
-    elif (ARI_score >= 10) & (ARI_score < 13): # if value is between 10 ~ 12.999999999
+    elif (ari_score >= 10) & (ari_score < 13):  # if value is between 10 ~ 12.999999999
         return 4
-    elif ARI_score >= 13: # if value is 13 or more
+    elif ari_score >= 13:  # if value is 13 or more
         return 5
 
 
@@ -175,13 +228,10 @@ def calculate_ari_array(tokens):
                             token is a list of sentence_tokens, each sentence in the input text is stored as one
                             sentence_token,
                                 each sentence_token stores each of its word as integer word_length
-    :return: list ARI_array which is containing the ARI score in each index
+    :complexity: O(r), where r is the number of tokens within tokens
+    :return: list ari_array which is containing the ARI score in each index
     """
-    ARI_array = []  # stores the ARI value, to be appended to the new last column is csv
-
-    #########debugging, for if there is a row without words, print the row
-    index = 0
-    #########debugging, if there is a row without words, print the row
+    ari_array = []  # stores the ARI value, to be appended to the new last column is csv
 
     for pre_processed_row_data in tokens:
         sum_of_words = 0
@@ -193,24 +243,20 @@ def calculate_ari_array(tokens):
             sum_of_words += len(j)
             sum_of_characters += sum(j)
 
-        #########debugging, if there is a row without words, print the row
-        if number_of_sentences == 0:
-            print(index)
-        if sum_of_words == 0:
-            print(index)
-        if sum_of_characters == 0:
-            print(index)
-        #########debugging, if there is a row without words, print the row
+        try:
+            if (number_of_sentences == 0) or (sum_of_words == 0):
+                raise ZeroDivisionError
+        except ZeroDivisionError as e:
+            raise SystemExit("Cell containing no words detected in csv file's last column, each cell in the last "
+                             "column must contain one or more word ... exiting".format(e)) from None
 
-        # calculate ARI score using our formula and append it to ARI_array
+        # calculate ARI score using our formula and append it to ari_array
         x = (0.5 * sum_of_words / number_of_sentences)
         y = (4.71 * sum_of_characters / sum_of_words)
-        ARI_value = x + y - 21.43
-        ARI_array.append(ARI_value)
+        ari_value = x + y - 21.43
+        ari_array.append(ari_value)
 
-        index += 1
-
-    return ARI_array
+    return ari_array
 
 
 def main_function(dataset_file_name):
@@ -220,6 +266,7 @@ def main_function(dataset_file_name):
     :precondition: dataset must be .csv
                    the text data must be in the last row for our dataset
     :param dataset_file_name: the .csv dataset's file name
+    :complexity: O(m), where m is length of the file.
     :return: tuple (header, data)
                 WHERE
                     header is the first header row to be added to our output csv in list format
@@ -231,6 +278,8 @@ def main_function(dataset_file_name):
     # read our csv file
     header, data = readfile(dataset_file_name)
 
+    print("Processing ...")
+
     # m is the index of the last cell of in a row
     # we assume all row are has the same number of columns in the CSV
     m = len(data[0]) - 1
@@ -238,66 +287,54 @@ def main_function(dataset_file_name):
     # note that each row is assumed to represent a document
     # for each document, tokenize its text
     tokens = []
-    for row in data:
-        text = row[m]
+    for cur_row in data:
+        text = cur_row[m]
         tokens.append(tokenize(text))
 
-    #print(data[0][m]) ### currently in use for debugging, delete it once its no longer needed
-    #print(tokens[0]) ### currently in use for debugging, delete it once its no longer needed
-
-    # calculate an array of ARI_scores using tokens
-    d_ARI_array = calculate_ari_array(tokens)
-
-    #print(d_ARI_array) ### currently in use for debugging, delete it once its no longer needed
-    #print(max(d_ARI_array)) ### currently in use for debugging, delete it once its no longer needed
+    # calculate an array of ari_scores using tokens
+    d_ari_array = calculate_ari_array(tokens)
 
     # add ARI column to a new column in our data
     i = 0
-    for row in data:
-        row.append(d_ARI_array[i])
+    for cur_row in data:
+        cur_row.append(d_ari_array[i])
         i += 1
 
     # update header for our data
     header.append("ARI")
 
-
     # add classification column to a new column in our data
     i = 0
-    for row in data:
-        classification = get_classification(d_ARI_array[i])
-        row.append(classification)
+    for cur_row in data:
+        classification = get_classification(d_ari_array[i])
+        cur_row.append(classification)
         i += 1
 
     # update header for our data
     header.append("Classification")
 
+    print("Finished processing")
     return header, data
 
 
 if __name__ == "__main__":
 
-    ### NOTE: we assume the file to be in (.csv) format, and it should also be in "utf-8" encoding   ###TEST we may want to test it is csv, if not, throw exception
-    #           * the last row of our csv file must store our text data     ###TEST? may want to throw exception is the input csv does not follow have the text data as law row?
-    #           * input csv should contain a header, or else, the first row of the data will be read as header and wont be included in the processing
+    the_dataset_file_name = sys.argv[1]  # line is for taking inputs from cmd, if we are using cmd
 
-    the_dataset_file_name = sys.argv[1]  #line is for taking inputs from cmd, if we are using cmd
-
-    # if u are not using cmd, comment the cmd line right above this and uncomment the line right below
-    # and input ur file name(.csv) below.
-    # the_dataset_file_name = "dataset WH.csv"
+    # # if u are not using cmd, comment the cmd line right above this and uncomment the line right below
+    # # and input ur file name(.csv) below.
+    # the_dataset_file_name = "test_non_utf_8.csv"
 
     # calculates ARI score and classification of the text in csv data, and return the data in the csv dataset with
-    # 2 new last rows, which is a new last row for ARI_score, followed by a new row for classification
+    # 2 new last rows, which is a new last row for ari_score, followed by a new row for classification
     d_header, d_data = main_function(the_dataset_file_name)
 
     # write our output csv
     output_file_name = "csv_output" + " " + the_dataset_file_name
     # write our new csv, with ARI added as a new last column, as output
-    with open(output_file_name, mode='w', newline='', encoding= "utf-8") as writing_file:
+    with open(output_file_name, mode='w', newline='', encoding="utf-8") as writing_file:
         file_writer = csv.writer(writing_file, delimiter=',')
 
         file_writer.writerow(d_header)
         for row in d_data:
             file_writer.writerow(row)
-
-
